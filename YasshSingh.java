e191e8df3b16872f12304f23a7b4045f79e27163
Yassh Singh <90470603+iyassh@users.noreply.github.com>
2021-09-17 02:02:05 +0530

    Add files via upload

---
+++ YasshSingh.java
@@ -0,0 +1,21 @@
+public class YasshSingh {
+    public static void main(String[] args) {
+        System.out.println("Good day everyone!\n");
+        System.out.println("My name is Yassh Singh and I am from Kolkata,India.\n");
+        System.out.println("I've always been intrigued by technology, especially when it comes to experimenting with new types of technology.\n");
+        System.out.println("Apart from that, my favourite pastimes are playing and watching sports, and I am also really intrigued by stock markets.\n ");
+        System.out.println(" ");
+        System.out.println(" .----------------.  .----------------. \n" +
+                "| .--------------. || .--------------. |\n" +
+                "| |  ____  ____  | || |    _______   | |\n" +
+                "| | |_  _||_  _| | || |   /  ___  |  | |\n" +
+                "| |   \\ \\  / /   | || |  |  (__ \\_|  | |\n" +
+                "| |    \\ \\/ /    | || |   '.___`-.   | |\n" +
+                "| |    _|  |_    | || |  |`\\____) |  | |\n" +
+                "| |   |______|   | || |  |_______.'  | |\n" +
+                "| |              | || |              | |\n" +
+                "| '--------------' || '--------------' |\n" +
+                " '----------------'  '----------------' ");
+
+    }
+}
